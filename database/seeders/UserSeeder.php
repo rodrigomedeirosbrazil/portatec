c4711e4622a1e464e701217dcf2d23325710e4b3
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-01-25 21:14:03 -0300

    Add UserSeeder with super-admin role and user creation

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create super-admin role
+        $superAdminRole = Role::create([
+            'name' => 'super-admin',
+            'guard_name' => 'web'
+        ]);
+
+        // Create super-admin user
+        $superAdmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'rodrigo@medeirostec.com.br',
+            'password' => Hash::make('123qweasd'),
+            'email_verified_at' => now(),
+        ]);
+
+        // Assign role to user
+        $superAdmin->assignRole($superAdminRole);
+    }
+}
