76d3d1ecc7720fcafac431b30e62620b0f9878a9
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-01-28 20:41:40 -0300

    Refactor Filament resources: Remove PropertyResource and add PlaceResource

---
+++ app/Filament/Resources/PlaceResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\App\Resources\PlaceResource as BaseResource;
+
+class PlaceResource extends BaseResource
+{
+}
--- app/Filament/Resources/PropertyResource.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Filament\Resources;
-
-use App\Filament\App\Resources\PropertyResource as BaseResource;
-
-class PropertyResource extends BaseResource
-{
-}
