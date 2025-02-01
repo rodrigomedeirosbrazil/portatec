18b43758c6d26a8cdd8043e01b1b2423e5616437
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-01-31 22:08:02 -0300

    fix: add missing methods to event works

--- app/Events/MqttMessageEvent.php
+++ app/Events/MqttMessageEvent.php
@@ -5,13 +5,21 @@ namespace App\Events;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
 
 class MqttMessageEvent
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     public function __construct(public string $topic, public string $message)
     {
         //
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
     }
 }
