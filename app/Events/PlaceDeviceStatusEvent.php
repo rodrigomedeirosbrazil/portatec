13f41f858cb5727ed8d40446b68f98703d830e8d
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-02-01 18:48:39 -0300

    feat: Implement PlaceDeviceStatusEvent for real-time device status broadcasting

---
+++ app/Events/PlaceDeviceStatusEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PlaceDeviceStatusEvent implements ShouldBroadcast
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public function __construct(public int $placeId, public int $deviceId, public string $status)
+    {
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel("Place.Device.Status.{$this->placeId}"),
+        ];
+    }
+}
