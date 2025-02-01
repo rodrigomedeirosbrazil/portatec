8ce5b6042f19bcc2f07b62bb15bd987f3a871ab8
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-02-01 18:48:22 -0300

    feat: Add broadcast channel for device status updates by place

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+
+Broadcast::channel('Place.Device.Status.{placeId}', function ($placeId) {
+    return true; // TODO: check if user has access to this place
+});
