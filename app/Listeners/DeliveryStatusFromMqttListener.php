d00a5b89954dae20e4529af4221c36350eac86cb
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-02-02 17:15:07 -0300

    feat: Implement MQTT device status and availability handling

--- app/Listeners/DeliveryStatusFromMqttListener.php
+++ app/Listeners/DeliveryStatusFromMqttListener.php
@@ -8,12 +8,28 @@ use App\Models\Device;
 use App\Models\PlaceDevice;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Str;
 
 class DeliveryStatusFromMqttListener implements ShouldQueue
 {
     use InteractsWithQueue;
 
     public function handle(MqttMessageEvent $event): void
+    {
+        if ($this->isLwtEvent($event)) {
+            $this->updateDevicesAvailability($event);
+            return;
+        }
+
+        $this->updateDevicesStatus($event);
+    }
+
+    public function isLwtEvent(MqttMessageEvent $event): bool
+    {
+        return Str::endsWith($event->topic, '/LWT');
+    }
+
+    public function updateDevicesStatus(MqttMessageEvent $event): void
     {
         Device::query()
             ->where('topic', $event->topic)
@@ -33,6 +49,29 @@ class DeliveryStatusFromMqttListener implements ShouldQueue
                         $placeDevice->place_id,
                         $device->id,
                         $payload
+                    )
+                );
+            });
+    }
+
+    public function updateDevicesAvailability(MqttMessageEvent $event): void
+    {
+        Device::query()
+            ->where('availability_topic', $event->topic)
+            ->with('placeDevices.place')
+            ->cursor()
+            ->each(function (Device $device) use ($event) {
+                $status = $event->message;
+
+                $device->is_available = $status === $device->availability_payload_on;
+                $device->save();
+
+                $device->placeDevices->each(fn (PlaceDevice $placeDevice) =>
+                    PlaceDeviceStatusEvent::dispatch(
+                        $placeDevice->place_id,
+                        $device->id,
+                        $device->is_available,
+                        $status
                     )
                 );
             });
