d00bdadf28c4ee0e1faa5f5a5efaec399eefb61a
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-02-02 09:11:16 -0300

    chore: Remove HTTPS scheme forcing in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
-
         Event::listen(
             MqttMessageEvent::class,
             DeliveryStatusFromMqttListener::class,
