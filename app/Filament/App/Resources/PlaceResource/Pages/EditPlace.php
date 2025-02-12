5840bf9d24c5f30997549a68f501b3072b4497c3
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-02-11 23:19:33 -0300

    feat: Add authorization check for place editing in EditPlace

--- app/Filament/App/Resources/PlaceResource/Pages/EditPlace.php
+++ app/Filament/App/Resources/PlaceResource/Pages/EditPlace.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\App\Resources\PlaceResource\Pages;
 
+use App\Enums\PlaceRoleEnum;
 use App\Filament\App\Resources\PlaceResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
@@ -15,5 +16,20 @@ class EditPlace extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function hasValidRecord(): bool
+    {
+        $hasValidRecord = parent::hasValidRecord();
+
+        if (!$hasValidRecord) {
+            return false;
+        }
+
+        return auth()->user()->hasRole('super_admin') ||
+            $this->record->placeUsers()
+                ->where('user_id', auth()->user()->id)
+                ->where('role', PlaceRoleEnum::Admin)
+                ->exists();
     }
 }
