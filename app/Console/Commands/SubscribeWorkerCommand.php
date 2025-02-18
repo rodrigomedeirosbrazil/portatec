bffc90c62cc8eb374c45c1a72ea275eca8a1e757
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-02-17 21:40:34 -0300

    refactor: Enhance MQTT subscribe worker to dynamically subscribe to device topics

--- app/Console/Commands/SubscribeWorkerCommand.php
+++ app/Console/Commands/SubscribeWorkerCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Events\MqttMessageEvent;
+use App\Models\Device;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use PhpMqtt\Client\Facades\MQTT;
@@ -14,35 +15,35 @@ class SubscribeWorkerCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'mqtt:subscribe-worker
-                            {--topic=}';
+    protected $signature = 'mqtt:subscribe-worker';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Subscribe to an MQTT topic and listen for messages';
+    protected $description = 'Subscribe to an MQTT topics';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $topic = $this->option('topic');
-
-        if (! $topic) {
-            $topic = '#';
-        }
-
-        $this->info("Subscribed to topic [$topic].");
-
         $mqtt = MQTT::connection();
-        $mqtt->subscribe($topic, function (string $topic, string $message) {
-            $this->info("$topic: $message");
-            Log::channel('mqtt-messages')->info("$topic: $message");
-            MqttMessageEvent::dispatch($topic, $message);
-        }, 0);
+
+        pcntl_async_signals(true);
+        pcntl_signal(SIGINT, fn () => $mqtt->interrupt());
+
+        Device::all()
+            ->pluck('topic')
+            ->unique()
+            ->each(function (string $topic) use ($mqtt) {
+                $mqtt->subscribe($topic, function (string $topic, string $message) {
+                    $this->info("$topic: $message");
+                    Log::channel('mqtt-messages')->info("$topic: $message");
+                    MqttMessageEvent::dispatch($topic, $message);
+                }, 0);
+            });
 
         $mqtt->loop(true);
     }
