e8f1c0f42cd551678bed024ecbbdf0dfba87b9dd
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-03-24 22:01:12 -0300

    feat: Implement device retrieval in TuyaService
    - Add getDevices method to TuyaService for fetching user devices from Tuya API
    - Utilize access token for authenticated requests
    - Include signing logic for API requests using HMAC-SHA256
    - Return device data or null based on API response success

--- app/Services/Tuya/TuyaService.php
+++ app/Services/Tuya/TuyaService.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Tuya;
 
 use App\DTOs\TuyaAuthenticationDTO;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Str;
 
 class TuyaService
 {
     private $clientId;
     private $clientSecret;
+    private $uid;
 
     private const API_URL = 'https://openapi.tuyaus.com';
 
@@ -17,6 +17,7 @@ class TuyaService
     {
         $this->clientId = env('TUYA_CLIENT_ID');
         $this->clientSecret = env('TUYA_CLIENT_SECRET');
+        $this->uid = env('TUYA_UID');
     }
 
     public function getAccessToken(): ?TuyaAuthenticationDTO
@@ -71,5 +72,56 @@ class TuyaService
             ->implode("\n");
 
         return join("\n", [$method, $sha256, $headersString, $urlPath]);
+    }
+
+    private function calculateSignWithAccessToken(string $timestamp, string $nonce, string $signStr, string $accessToken)
+    {
+        $str = $this->clientId . $accessToken . $timestamp . $nonce . $signStr;
+        $hash = hash_hmac('sha256', $str, $this->clientSecret, false);
+        return strtoupper($hash);
+    }
+
+    public function getDevices()
+    {
+        // $authenticationDTO = $this->getAccessToken();
+
+        // if (! $authenticationDTO) {
+        //     throw new \Exception('Failed to get access token');
+        // }
+
+        $authenticationDTO = new TuyaAuthenticationDTO(
+            accessToken: 'daa100ca50f303d763889a4eae2e033a',
+            refreshToken: 'e8324d442ed677aa2afc7d376bd40ce5',
+            expireTime: 7200,
+            uid: 'bay1647829239658KekG',
+        );
+
+        $urlPath = "/v1.0/users/{$this->uid}/devices";
+
+        $method = 'GET';
+        $headers = [];
+        $body = '';
+        $signStr = $this->calcSignString($method, $headers, $body, $urlPath);
+
+        $timestamp = now()->timestamp * 1000;
+        $nonce = '';
+
+        $sign = $this->calculateSignWithAccessToken($timestamp, $nonce, $signStr, $authenticationDTO->accessToken);
+
+        $response = Http::withHeaders([
+            'client_id' => $this->clientId,
+            'access_token' => $authenticationDTO->accessToken,
+            'sign' => $sign,
+            't' => $timestamp,
+            'sign_method' => 'HMAC-SHA256',
+        ])->send($method, self::API_URL . $urlPath);
+
+        if ($response->successful()) {
+            $data = json_decode($response->body(), true);
+            return $data;
+        }
+
+        dump($response->status(), $response->body());
+        return null;
     }
 }
