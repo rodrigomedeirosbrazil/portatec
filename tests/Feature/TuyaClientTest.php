71b37bae3097f373c2e39072dccec466f2884d64
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-03-26 21:38:12 -0300

    test: Update TuyaClientTest with expected values and null body
    - Adjust expected string request in testStringRequest for clarity
    - Change body parameter from empty array to null in getStringRequest call
    - Update accessToken and expectedSign values in testSignString for consistency
    - Modify timestamp value in testSignString to reflect current test scenario

--- tests/Feature/TuyaClientTest.php
+++ tests/Feature/TuyaClientTest.php
@@ -12,18 +12,20 @@ class TuyaClientTest extends TestCase
     public function testStringRequest(): void
     {
         $uid = 'az1742250055143BSJGs';
+        $expectedStringRequest = 'GET' . PHP_EOL . 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' . PHP_EOL . '' . PHP_EOL . "/v1.0/users/{$uid}/devices";
+
         Http::fake();
         $client = new Client();
 
         $stringRequest = $client->getStringRequest(
             method: Request::METHOD_GET,
             headers: [],
-            body: [],
+            body: null,
             urlPath: "/v1.0/users/{$uid}/devices",
         );
 
         $this->assertEquals(
-            'GET' . PHP_EOL . 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' . PHP_EOL . '' . PHP_EOL . "/v1.0/users/{$uid}/devices",
+            $expectedStringRequest,
             $stringRequest
         );
     }
@@ -33,9 +35,9 @@ class TuyaClientTest extends TestCase
         $clientId = 'twfspdm9p5mkvtxe9y8n';
         $clientSecret = '12f3598e08484b518eaf495ae249ef9f';
         $uid = 'az1742250055143BSJGs';
-        $accessToken = '3d6180a39a635aec3c3c0728e17aae72';
+        $accessToken = '7c59621407dce5eeada4fd5f2e0ac548';
 
-        $expectedSign = 'A3A3BAB4EF40A043435781E5DB82D7C573CCBA3BDB2570CEC0C98F2339A091C2';
+        $expectedSign = '9D5D3F8C0F97002A1DC9A1EBB40FF0E5D17399A2ADEDA2AB414EC1CF94A30588';
 
         $stringRequest = 'GET'
             . PHP_EOL
@@ -48,7 +50,7 @@ class TuyaClientTest extends TestCase
         Http::fake();
         $client = new Client();
 
-        $timestamp = '1742953912555';
+        $timestamp = '1743034828181';
         $nonce = '';
 
         $sign = $client->getSignString(
