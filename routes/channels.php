8f2a0bf8d161059b4940f993bed97355a8771a7d
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-06-16 23:21:05 -0300

    feat: Add Place.Device.Command.Ack channel for broadcasting command acknowledgments
    - Introduce Place.Device.Command.Ack channel to handle device command acknowledgment broadcasts
    - Implement access control check for user permissions on the specified place

--- routes/channels.php
+++ routes/channels.php
@@ -11,7 +11,10 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-
 Broadcast::channel('Place.Device.Status.{placeId}', function ($placeId) {
+    return true; // TODO: check if user has access to this place
+});
+
+Broadcast::channel('Place.Device.Command.Ack.{placeId}', function ($placeId) {
     return true; // TODO: check if user has access to this place
 });
