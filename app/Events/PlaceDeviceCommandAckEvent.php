6e06064e9668a0dcfd104e344235e0e5870bf5f1
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-06-16 23:20:23 -0300

    feat: Add PlaceDeviceCommandAckEvent for broadcasting device command acknowledgments
    - Introduce PlaceDeviceCommandAckEvent class implementing ShouldBroadcast interface
    - Define properties for place ID, device ID, and command
    - Implement broadcastOn method to specify the private channel for broadcasting

---
+++ app/Events/PlaceDeviceCommandAckEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PlaceDeviceCommandAckEvent implements ShouldBroadcast
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public function __construct(
+        public int $placeId,
+        public int $deviceId,
+        public string $command,
+    ) {}
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel("Place.Device.Command.Ack.{$this->placeId}"),
+        ];
+    }
+}
