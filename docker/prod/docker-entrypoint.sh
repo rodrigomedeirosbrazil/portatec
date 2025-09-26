67c552f117c8a3a578d9bd515446a2b7ab78b924
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-09-25 22:27:41 -0300

    chore: Move npm dependency installation and asset building to Dockerfile for improved efficiency

--- docker/prod/docker-entrypoint.sh
+++ docker/prod/docker-entrypoint.sh
@@ -3,14 +3,6 @@
 # echo "Changing Nginx default port to ${HTTP_NGINX_PORT}/${HTTPS_NGINX_PORT}"
 # /usr/bin/envsubst '$HTTP_NGINX_PORT,$HTTPS_NGINX_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-echo "Installing npm dependencies..."
-cd /var/www && npm ci
-
-echo "Building Vite assets..."
-cd /var/www && npm run build
-
-echo "Running optimize command..."
-/usr/local/bin/php /var/www/artisan optimize
 
 echo "Starting Supervisor..."
 /usr/bin/supervisord -c /etc/supervisord.conf
