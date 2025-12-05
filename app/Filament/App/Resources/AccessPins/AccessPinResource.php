7a3e97356ef3cc2cd4eb54199b910c020fa923ba
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-12-05 20:39:26 -0300

    feat: Implement Filament AccessPinResource

---
+++ app/Filament/App/Resources/AccessPins/AccessPinResource.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Filament\App\Resources\AccessPins;
+
+use App\Filament\App\Resources\AccessPins\Pages\ManageAccessPins;
+use App\Models\AccessPin;
+use App\Models\Place;
+use App\Models\User;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class AccessPinResource extends Resource
+{
+    protected static ?string $model = AccessPin::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-key';
+
+    protected static ?string $recordTitleAttribute = 'pin';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('place_id')
+                    ->label('Place')
+                    ->options(Place::all()->pluck('name', 'id'))
+                    ->required()
+                    ->searchable(),
+                Select::make('user_id')
+                    ->label('User')
+                    ->options(User::all()->pluck('name', 'id'))
+                    ->required()
+                    ->searchable(),
+                TextInput::make('pin')
+                    ->required()
+                    ->numeric()
+                    ->minLength(6)
+                    ->maxLength(6)
+                    ->mask('999999'),
+                DateTimePicker::make('start')
+                    ->label('Start')
+                    ->required(),
+                DateTimePicker::make('end')
+                    ->label('End')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('place.name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('user.name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('pin')
+                    ->searchable(),
+                TextColumn::make('start')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('end')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageAccessPins::route('/'),
+        ];
+    }
+}
