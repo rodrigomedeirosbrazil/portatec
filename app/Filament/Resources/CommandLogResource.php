464732220f225bcced7abd25d774fa9caf8d8de3
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2026-02-27 14:46:58 -0300

    fix(import): update DatePicker import path in AccessEventResource and CommandLogResource

--- app/Filament/Resources/CommandLogResource.php
+++ app/Filament/Resources/CommandLogResource.php
@@ -11,7 +11,7 @@ use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
-use Filament\Forms\DatePicker;
+use Filament\Forms\Components\DatePicker;
 use Filament\Resources\Resource;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
